258ffc73bb426fd55fbf86a9c11aab50c9333270
yavuz <yavuzkaand@hotmail.com>
2020-03-25 12:28:49 +0100

    Pokemon class init

---
+++ pokemon.py
@@ -0,0 +1,9 @@
+class Pokemon:
+    def __init__(self, name, energyType, maxHealth, health, attacks, weakness, resistance):
+        self.name = name
+        self.energyType = energyType
+        self.maxHealth = maxHealth
+        self.health = health
+        self.attacks = attacks
+        self.weakness = weakness
+        self.resistance = resistance
